42eb71cf0730f27299ac7afdd97a3bb432ea24e3
BBagdanDan <161213746+BBagdanDan@users.noreply.github.com>
2025-05-29 12:18:56 +0200

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  rules: {
+    // твої правила або залиш як є
+  },
+};
